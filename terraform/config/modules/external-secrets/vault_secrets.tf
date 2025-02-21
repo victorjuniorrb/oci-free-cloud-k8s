15d44e378d49f3a8d543e8c7ffeb7379f446679e
Victor Júnior R. B. <victorjunior@gmail.com>
2025-02-21 00:39:48 -0300

    chore: ajust kind on apiVersion: external-secrets.io/v1beta1

--- terraform/config/modules/external-secrets/vault_secrets.tf
+++ terraform/config/modules/external-secrets/vault_secrets.tf
@@ -26,7 +26,7 @@ YAML
 resource "kubectl_manifest" "external_secrets_cluster_store" {
   yaml_body = <<YAML
 apiVersion: external-secrets.io/v1beta1
-kind: ClusterSecretStore
+kind: SecretStore
 metadata:
   name: oracle-vault
 spec:
